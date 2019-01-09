a3f45e6c05d35d738d14cac4b91a521158d34a80
Sergey Stadnik <sstadnik@odecee.com.au>
2019-01-09 22:18:36 +1100

    Added CSS Transition demo

--- src/CSSTransitionDemo.js
+++ src/CSSTransitionDemo.js
@@ -2,21 +2,58 @@
 
 import * as React from 'react';
 import ReactDOM from 'react-dom';
-import classnames from 'classnames';
-import { ReactComponent as SearchIcon } from './search.svg';
 // import './styles.css';
+
+/* This CSS style is applied when the drawer is opening and opened*/
+const openingStyle = {
+  maxWidth: '100%' /* max-with is 100% when the drawer is opened */,
+  opacity: 1 /* opened drawer is opaque */,
+  /* Upon transitioning to Open,
+     animate `max-width' for 0.5s
+     and 'opacity' for 0.2s*/
+  transition: 'max-width 0.5s, opacity 0.2s'
+};
+
+/* This CSS style is applied when the drawer is closing and closed */
+const closingStyle = {
+  maxWidth: 0 /* max-width is 0 in the closed drawer */,
+  opacity: 0 /* closed drawer is transparent */,
+  /* Upon transitioning Closed to Open,
+     animate `max-width' for 0.5s
+     and 'opacity' for 0.2s  with 0.3s delay */
+  transition: 'max-width 0.5s, opacity 0.2s 0.3s'
+};
 
 class Search extends React.Component {
   state = {
     opened: false // Initially search form is Closed
   };
 
+  toggleOpened = () =>
+    // Toggle opened / closed state.
+    // Because we rely on the previous state, we need to use a function setState form
+    // https://ozmoroz.com/2018/11/why-my-setstate-doesnt-work/
+    this.setState(state => ({ ...state, opened: !state.opened }));
+
   render() {
+    const { opened } = this.state;
     return (
-      <div className="search-form">
-        <input type="text" />
-        <button type="button">
-          <SearchIcon className="search-icon" />
+      <div className="drawer-form col-12 col-md-4">
+        <input
+          type="text"
+          // 'form__input' class defined in the CSS file
+          // applies 'transition' CSS property
+          className="form__input"
+          // Apply 'openingStyle' CSS class if the drawer is opened,
+          // and 'closingStyle' if the drawer is closed.
+          style={opened ? openingStyle : closingStyle}
+        />
+        <button
+          type="button"
+          className="form__button btn btn-primary"
+          onClick={this.toggleOpened}
+        >
+          {opened ? 'Close' : 'Open'}
         </button>
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -5,13 +5,16 @@ import { default as CSSTransitionDemo } from './CSSTransitionDemo';
 import './styles.css';
 
 function App() {
-    return (
-        <div className="App">
-            <h1>Hello CodeSandbox</h1>
-            <h2>CSS Transition demo</h2>
-            <CSSTransitionDemo />
-        </div>
-    );
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>CSS Transition demo</h2>
+      <p>
+        Animate CSS <code>max-width</code> and <code>opacity</code> properties
+      </p>
+      <CSSTransitionDemo />
+    </div>
+  );
 }
 
 const rootElement = document.getElementById('root');
