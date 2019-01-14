2d91a2b73bdff6d675e21021c65406745bf39159
Sergey Stadnik <sstadnik@odecee.com.au>
2019-01-14 11:44:45 +1100

    Added opacity animation

--- src/CSSTransitionDemo.js
+++ src/CSSTransitionDemo.js
@@ -5,17 +5,19 @@ import * as React from 'react';
 /* This CSS style is applied when the drawer is opened */
 const openedStyle = {
   maxWidth: '100%' /* max-with is 100% when the drawer is opened */,
+  opacity: 1 /* opened drawer is opaque */,
   /* Upon transitioning to Open,
-     animate `max-width' for 0.5sv*/
-  transition: 'max-width 0.5s'
+     animate `max-width' for 0.5s and opacity for 0.2s */
+  transition: 'max-width 0.5s, opacity 0.2s'
 };
 
 /* This CSS style is applied when the drawer is closed */
 const closedStyle = {
   maxWidth: 0 /* max-width is 0 in the closed drawer */,
+  opacity: 0 /* closed drawer is transparent */,
   /* Upon transitioning to Closed,
-     animate `max-width' for 0.5s */
-  transition: 'max-width 0.5s'
+     animate `max-width' for 0.5s and opacity for 0.2s */
+  transition: 'max-width 0.5s, opacity 0.2s'
 };
 
 class Drawer extends React.Component {
